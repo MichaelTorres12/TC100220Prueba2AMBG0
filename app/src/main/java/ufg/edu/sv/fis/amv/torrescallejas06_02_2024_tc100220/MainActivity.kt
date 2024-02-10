da59714e1b694d560ba6fd131251e2e8c1785a24
MichaelTorres12 <ia.michaeltorrescallejas@gmail.com>
2024-02-09 22:13:01 -0600

    Primer commir se me olvido quitar el .gitignore

---
+++ app/src/main/java/ufg/edu/sv/fis/amv/torrescallejas06_02_2024_tc100220/MainActivity.kt
@@ -0,0 +1,11 @@
+package ufg.edu.sv.fis.amv.torrescallejas06_02_2024_tc100220
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TorresCallejas06-02-2024-TC100220"
+include(":app")
